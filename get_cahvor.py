360b84a4f2ad1519e789a6bbdbec34625fab7f48
Bhavin Nayak <bhavin.nayak@asu.edu>
2016-04-22 11:30:56 -0700

    changes made in cahvor code structure.

--- get_cahvor.py
+++ get_cahvor.py
@@ -1,45 +1,38 @@
 import numpy as np
 import math
+from compute_coordinates import compute_coordinates
 
 
-class PhotogrammetricModel(object):
+class CAHVOR(object):
 
-    def __init__(self, dict_ip=None):
-        if dict_ip is None:
-            print("----------------------------------------------------------")
-            print('Note: Enter Vector Inputs Row by Row with Space in between')
-            print("----------------------------------------------------------")
-            print("")
-            self.principal = input('Enter Principal Point (x0, y0): ')
-            self.principal = [float(x) for x in self.principal.split()]
-            self.imsize = input('Enter Image Size (Row, Column): ')
-            self.imsize = [float(x) for x in self.imsize.split()]
-            self.focallength = float(input('Enter Focal Length in mm: '))
-            self.center = input('Enter Camera Center (Xc, Yc, Zc): ')
-            self.center = [float(x) for x in self.center.split()]
-            self.pixelsize = float(input('Enter pixel size in mm: '))
-            w = float(input('Enter Rotation Angle w: '))
-            phi = float(input('Enter Rotation Angle phi: '))
-            k = float(input('Enter Rotation Angle k: '))
-            self.distortion = input('Enter Distortion Parameters (k0, k1, k2): ')
-            self.distortion = [float(x) for x in self.distortion.split()]
-            print("")
-            self.r_matrix = compute_rotation_matrix(w, phi, k)
-            pinhole_model = dict([('C', self.center),
-                                  ('imsize', self.imsize),
-                                  ('pixel_size', self.pixelsize),
-                                  ('r_mat', self.r_matrix),
-                                  ('f', self.focallength),
-                                  ('P', self.principal),
-                                  ('K', self.distortion)])
-        else:
-            pinhole_model = dict([('C', dict_ip['center']),
-                                  ('imsize', dict_ip['image_size']),
-                                  ('pixel_size', dict_ip['pixelsize']),
-                                  ('r_mat', dict_ip['rotation_mat']),
-                                  ('f', dict_ip['f']),
-                                  ('P', dict_ip['principal']),
-                                  ('K', None)])
+    def __init__(self):
+        print("----------------------------------------------------------")
+        print('Note: Enter Vector Inputs Row by Row with Space in between')
+        print("----------------------------------------------------------")
+        print("")
+        self.principal = input('Enter Principal Point (x0, y0): ')
+        self.principal = [float(x) for x in self.principal.split()]
+        self.imsize = input('Enter Image Size (Row, Column): ')
+        self.imsize = [float(x) for x in self.imsize.split()]
+        self.focallength = float(input('Enter Focal Length in mm: '))
+        self.center = input('Enter Camera Center (Xc, Yc, Zc): ')
+        self.center = [float(x) for x in self.center.split()]
+        self.pixelsize = float(input('Enter pixel size in mm: '))
+        w = float(input('Enter Rotation Angle w: '))
+        phi = float(input('Enter Rotation Angle phi: '))
+        k = float(input('Enter Rotation Angle k: '))
+        self.distortion = input('Enter Distortion Parameters (k0, k1, k2): ')
+        self.distortion = [float(x) for x in self.distortion.split()]
+        print("")
+        self.r_matrix = compute_rotation_matrix(w, phi, k)
+        pinhole_model = dict([('center', self.center),
+                              ('image_size', self.imsize),
+                              ('pixelsize', self.pixelsize),
+                              ('rotation_mat', self.r_matrix),
+                              ('f', self.focallength),
+                              ('principal', self.principal),
+                              ('K', self.distortion)
+                              ('az', 0), ('el', 0)])
 
         self.cahvor = compute_CAHVOR(pinhole_model)
         print_CAHVOR(self.cahvor)
@@ -65,11 +58,15 @@ def compute_rotation_matrix(w, phi, k):
     # Rotational Matrix M generation
     r_matrix = np.zeros((3, 3))
     r_matrix[0, 0] = math.cos(phi) * math.cos(k)
-    r_matrix[0, 1] = math.sin(w) * math.sin(phi) * math.cos(k) + math.cos(w) * math.sin(k)
-    r_matrix[0, 2] = - math.cos(w) * math.sin(phi) * math.cos(k) + math.sin(w) * math.sin(k)
+    r_matrix[0, 1] = math.sin(w) * math.sin(phi) * math.cos(k) + \
+        math.cos(w) * math.sin(k)
+    r_matrix[0, 2] = - math.cos(w) * math.sin(phi) * math.cos(k) + \
+        math.sin(w) * math.sin(k)
     r_matrix[1, 0] = - math.cos(phi) * math.sin(k)
-    r_matrix[1, 1] = - math.sin(w) * math.sin(phi) * math.sin(k) + math.cos(w) * math.cos(k)
-    r_matrix[1, 2] = math.cos(w) * math.sin(phi) * math.sin(k) + math.sin(w) * math.cos(k)
+    r_matrix[1, 1] = - math.sin(w) * math.sin(phi) * math.sin(k) + \
+        math.cos(w) * math.cos(k)
+    r_matrix[1, 2] = math.cos(w) * math.sin(phi) * math.sin(k) + \
+        math.sin(w) * math.cos(k)
     r_matrix[2, 0] = math.sin(phi)
     r_matrix[2, 1] = - math.sin(w) * math.cos(phi)
     r_matrix[2, 2] = math.cos(w) * math.cos(phi)
@@ -78,23 +75,43 @@ def compute_rotation_matrix(w, phi, k):
 
 
 def compute_CAHVOR(pinhole_model):
-    hs = pinhole_model['f'] / pinhole_model['pixel_size']
-    vs = pinhole_model['f'] / pinhole_model['pixel_size']
-    hc = (pinhole_model['imsize'][1] / 2) + (pinhole_model['P'][0] / pinhole_model['pixel_size'])
-    vc = (pinhole_model['imsize'][0] / 2) - (pinhole_model['P'][1] / pinhole_model['pixel_size'])
+    hs = pinhole_model['f'] / pinhole_model['pixelsize']
+    vs = pinhole_model['f'] / pinhole_model['pixelsize']
+    hc = (pinhole_model['image_size'][1] / 2) + \
+         (pinhole_model['principal'][0] / pinhole_model['pixelsize'])
+    vc = (pinhole_model['image_size'][0] / 2) - \
+         (pinhole_model['principal'][1] / pinhole_model['pixelsize'])
 
-    C = pinhole_model['C']
-    A = - pinhole_model['r_mat'][2, :]
-    Hn = pinhole_model['r_mat'][0, :]
-    Vn = - pinhole_model['r_mat'][1, :]
+    C = pinhole_model['center']
+    A = - pinhole_model['rotation_mat'][2, :]
+    Hn = pinhole_model['rotation_mat'][0, :]
+    Vn = - pinhole_model['rotation_mat'][1, :]
 
     H = hs * Hn + hc * A
     V = vs * Vn + vc * A
     O = A        # We assume O = A in converted CAHVOR Model
-    R = pinhole_model['K']
-    if not(R is None):
+
+    # Fixing Axis specifically for PTU unit.
+    A[0], A[1], A[2] = A[2], -A[0], -A[1]
+    H[0], H[1], H[2] = H[2], -H[0], -H[1]
+    V[0], V[1], V[2] = V[2], -V[0], -V[1]
+    O[0], O[1], O[2] = O[2], -O[0], -O[1]
+
+    A = compute_coordinates(A, pinhole_model['az'], pinhole_model['el'])
+    H = compute_coordinates(H, pinhole_model['az'], pinhole_model['el'])
+    V = compute_coordinates(V, pinhole_model['az'], pinhole_model['el'])
+    O = compute_coordinates(O, pinhole_model['az'], pinhole_model['el'])
+
+    try:
+        R = pinhole_model['K']
+    except KeyError:
+        R = None
+
+    if not (R is None):
         R = np.array([R[0], R[1] * (pinhole_model['f']**2),
                       R[2] * (pinhole_model['f']**4)])
+        R = compute_coordinates(R, pinhole_model['az'], pinhole_model['el'])
+
     cahvor = dict([('C', C), ('A', A), ('H', H), ('V', V), ('O', O), ('R', R),
                    ('hs', hs), ('hc', hc), ('vs', vs), ('vc', vc)])
     return cahvor
@@ -118,4 +135,4 @@ def print_CAHVOR(cahvor):
     print("--------------------------------------------------------------")
 
 if __name__ == '__main__':
-    cahvor = PhotogrammetricModel()
+    cahvor = CAHVOR()
